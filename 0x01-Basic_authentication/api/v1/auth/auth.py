2406b6dd79adae8605b65ac88750bdc8ce5a9ceb
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-08-09 16:07:59 +0000

    pycodestyled

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,20 +5,18 @@ from flask import request
 from typing import List, TypeVar
 from os import getenv
 
+
 class Auth:
     """Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-         """Checks if authentication is required for the given path."""
-         return False
-    
+        """Checks if authentication is required for the given path."""
+        return False
 
     def authorization_header(self, request=None) -> str:
         """Returns the value of the Authorization header."""
         return None
 
-    
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None - not implemented."""
         return None
-        
