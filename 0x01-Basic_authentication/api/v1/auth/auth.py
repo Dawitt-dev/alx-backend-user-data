c453a7add2a733e577a05a25694b43164ad15871
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-08-09 17:15:13 +0000

    task_5. Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Returns the value of the Authorization header."""
-        return None
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None - not implemented."""
