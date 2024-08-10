02f4435720239e2f93c9aee86504e776f7467706
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-08-10 01:24:28 +0000

    task_13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -33,8 +33,8 @@ class Auth:
         """ require auth method """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        if path[-1] != '/':
-            path += '/'
+        if path[-1] != '*':
+            path += '*'
         if path in excluded_paths:
             return False
         return True
