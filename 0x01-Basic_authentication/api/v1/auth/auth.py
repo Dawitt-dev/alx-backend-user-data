8e36407dd826cdc0353940436f8753475df66320
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-08-10 01:20:48 +0000

    task_13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,4 +29,12 @@ class Auth:
         """Returns None - not implemented."""
         return None
 
-
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if authentication is required for the given path."""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
